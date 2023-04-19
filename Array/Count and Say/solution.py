993137af47ceb71d309378efae15e7466ec1f6ae
Phong Phi Gia <tpw284@gmail.com>
2023-04-20 00:08:10 +0700

    Count and Say

--- Array/Count and Say/solution.py
+++ Array/Count and Say/solution.py
@@ -2,21 +2,14 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
-        result = "1"
-        for i in range(2, n + 1):
-            queue = list()
-            for char in result:
-                if len(queue) == 0:
-                    queue.append((char, 1))
-                else:
-                    last = queue.pop(-1)
-                    if last[0] == char:
-                        queue.append((char, last[1] + 1))
-                    else:
-                        queue.append(last)
-                        queue.append((char, 1))
+        prev = self.countAndSay(n - 1)
 
-            result = ""
-            for item in queue:
-                result = result + str(item[1]) + item[0]
+        count = 1
+        result = ""
+        for i in range(len(prev)):
+            if i == len(prev) - 1 or prev[i] != prev[i + 1]:
+                result = result + str(count) + prev[i]
+                count = 1
+            else:
+                count += 1
         return result
