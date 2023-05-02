f6d6714e58ad9ee25257a3cfc256fedcb5a03990
Phong Phi <tpw284@gmail.com>
2023-05-02 23:58:35 +0700

    Reveal Cards In Increasing Order

---
+++ Array/Reveal Cards In Increasing Order/solution.ts
@@ -0,0 +1,15 @@
+function deckRevealedIncreasing(deck: number[]): number[] {
+    deck.sort((a,b) => a-b)
+    let result = []
+    helper(deck, result)
+    return result
+
+};
+
+function helper(deck, result = []) {
+    if(!deck.length) return
+    if(result.length)
+    result.unshift(result.pop())
+    result.unshift(deck.pop())
+    helper(deck, result)
+}
