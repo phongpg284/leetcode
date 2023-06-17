cc6307ddec536a6435ed6063c5a835386e49c072
Phong Phi Gia <tpw284@gmail.com>
2023-06-17 18:11:29 +0700

    K Radius Subarray Averages

---
+++ Array/K Radius Subarray Averages/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        res = [-1] * n
+        temp = 0
+
+        if k == 0:
+            return nums
+        
+        if k * 2 >= n:
+            return res
+        
+        for i in range(k * 2 + 1):
+            temp += nums[i]
+        res[k] = int(temp / (2*k + 1) * 10 // 10)
+        
+        for i in range(k + 1, n - k):
+            temp += nums[i+k] - nums[i-k-1]
+            res[i] = int(temp / (2*k + 1) * 10 // 10)
+        return res    
