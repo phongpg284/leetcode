bb9f8441c2680690a28d2ef1f56ed401e597c388
Phong Phi Gia <tpw284@gmail.com>
2023-06-18 19:12:11 +0700

    K Radius Subarray Averages

--- Array/K Radius Subarray Averages/solution.py
+++ Array/K Radius Subarray Averages/solution.py
@@ -10,10 +10,12 @@ class Solution:
         if k * 2 >= n:
             return res
         
+        # Traverse first k-radius
         for i in range(k * 2 + 1):
             temp += nums[i]
         res[k] = int(temp / (2*k + 1) * 10 // 10)
         
+        # Shift k-radius to the end
         for i in range(k + 1, n - k):
             temp += nums[i+k] - nums[i-k-1]
             res[i] = int(temp / (2*k + 1) * 10 // 10)
