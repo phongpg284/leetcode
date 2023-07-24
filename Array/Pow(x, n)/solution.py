8e4794cbbfc439ed146d16bf0551c148c096f807
Phong Phi Gia <tpw284@gmail.com>
2023-07-24 23:58:15 +0700

    Pow(x, n)

--- Array/Pow(x, n)/solution.py
+++ Array/Pow(x, n)/solution.py
@@ -3,6 +3,7 @@ class Solution:
         if n == 0:
             return 1
         
+        # mark if n is negative
         is_negative = False
         if n < 0:
             is_negative = True
@@ -13,13 +14,14 @@ class Solution:
         while n > 0:
             cur = x
             count = 1
-
+            # multiple until meet x^n using cur ** 2 to reduce time complecity to log n)            
             while count * 2 <= n:
                 count *= 2
                 cur = cur * cur
             n-= count
             res *= cur
 
+        # if n is negative return 1/res
         if is_negative:
             return 1/res
         return res        
