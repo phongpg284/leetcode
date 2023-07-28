534d5a7e22a70e6f7a84ba1248ba1fb02cc83096
Phong Phi Gia <tpw284@gmail.com>
2023-07-28 21:28:18 +0700

    Predict the Winner

--- DP/Predict the Winner/solution.py
+++ DP/Predict the Winner/solution.py
@@ -1,12 +1,17 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        result = [[0] * len(nums) for i in range(len(nums))]
-        for i in range(len(nums) - 1, -1, -1):
-            for j in range(i, len(nums)):
+        n = len(nums)
+        result = [[0] * n for _ in range(n)]    
+        '''
+            points difference player go first will get with nums from i to j =>  
+            => if result[i][j] > 0 means player 1 win
+        '''
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
                 if i == j:
                     result[i][j] = nums[i]
                 else:
-                    left = nums[i] - result[i+1][j] 
-                    right = nums[j] - result[i][j-1] 
+                    left = nums[i] - result[i+1][j]     # case where player pick nums[i]
+                    right = nums[j] - result[i][j-1]    # case where player pick nums[j]
                     result[i][j] = max(left, right)
-        return result[0][len(nums) - 1] >= 0
+        return result[0][n - 1] >= 0
