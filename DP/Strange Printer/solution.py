68481e1e8cb1d1646f42946aeb0f8f7ad648a7de
Phong Phi Gia <tpw284@gmail.com>
2023-07-30 16:25:46 +0700

    Strange Printer

--- DP/Strange Printer/solution.py
+++ DP/Strange Printer/solution.py
@@ -2,12 +2,12 @@ class Solution:
     def strangePrinter(self, s: str) -> int:
         n = len(s)
         dp = [[0] * n for _ in range(n)]
-
+        # minimum turns for string from i to j
         for i in range(n - 1, -1, -1):
-            dp[i][i] = 1
+            dp[i][i] = 1                # default case 
             for j in range(i + 1, n):
-                dp[i][j] = dp[i][j - 1] + 1
-                for k in range(i, j):
-                    if s[k] == s[j]:
+                dp[i][j] = dp[i][j - 1] + 1     # default case with one more turn for last character
+                for k in range(i, j):           
+                    if s[k] == s[j]:            # check if character match last character then can replace without new turn 
                         dp[i][j] = min(dp[i][j], dp[i][k] + (dp[k + 1][j - 1] if k + 1 <= j - 1 else 0))
         return dp[0][n - 1]
