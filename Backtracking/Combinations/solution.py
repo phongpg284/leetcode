e8d66f6654f2f795e13d198260680253ba6bf576
Phong Phi Gia <tpw284@gmail.com>
2023-08-01 23:52:53 +0700

    Combinations

--- Backtracking/Combinations/solution.py
+++ Backtracking/Combinations/solution.py
@@ -1,3 +1,19 @@
+# Normal Recursive solution
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        self.recur(n, k, 0, [], res)
+        return res
+    def recur(self, n, k, i, group, res): 
+        if len(group) == k:
+            res.append(group)
+        for j in range(i+1, n+1):
+            new_group = group[:]
+            new_group.append(j)
+            self.recur(n, k, j, new_group, res)
+
+
+# Back track solution
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
