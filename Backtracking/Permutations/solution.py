2dd77578d30ef4ac6746208de6547994f9ddf300
Phong Phi Gia <tpw284@gmail.com>
2023-08-02 22:18:22 +0700

    Permutations

--- Backtracking/Permutations/solution.py
+++ Backtracking/Permutations/solution.py
@@ -1,18 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        mark = [False] * len(nums)
-        return self.permulation_recursive([], mark, nums, []) 
+        n = len(nums)
+        visited = [False] * n
+        res = []
+        
+        def backtrack(temp):
+            # Form a permutation
+            if len(temp) == n:
+                res.append(temp[:])
+                return
 
-    def permulation_recursive(self, result, mark, nums, res):
-        if len(result) == len(nums):
-            res.append(result)
-            return res
-
-        for i in range(len(mark)):
-            if mark[i] == False:
-                new_mark = mark.copy()
-                new_mark[i] = True
-                new_result = result[::] 
-                new_result.append(nums[i])
-                self.permulation_recursive(new_result, new_mark, nums, res)
+            for i in range(n):
+                # Recur on not pick nums 
+                if not visited[i]:
+                    # Pick into current temp permutation
+                    visited[i] = True
+                    temp.append(nums[i])
+                    
+                    backtrack(temp)
+                    
+                    # Unpic from current temp permutation
+                    visited[i] = False
+                    temp.pop()
+        
+        backtrack([]) 
         return res
