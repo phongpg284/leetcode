7767781ebe75adcc50e13ffc70496877ecd05c43
Phong Phi Gia <tpw284@gmail.com>
2023-08-07 23:47:38 +0700

    Search a 2D Matrix

--- Binary Search/Search a 2D Matrix/solution.py
+++ Binary Search/Search a 2D Matrix/solution.py
@@ -1,17 +1,16 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        low = 0
-        high = m * n - 1
+        m, n = len(matrix), len(matrix[0])
+        low, high = 0, m * n - 1
+
         while low <= high:
-            middle = (low + high) // 2
-            row = middle // n
-            col = middle - row * n
+            mid = (low + high) // 2
+            row = mid // n
+            col = mid % n
             if matrix[row][col] == target:
                 return True
             elif matrix[row][col] > target:
-                high = middle - 1
+                high = mid - 1
             else:
-                low = middle + 1
+                low = mid + 1
         return False
