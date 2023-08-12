72cda40e307efe6e0e9256c4d25ebac7e728bf48
Phong Phi Gia <tpw284@gmail.com>
2023-08-12 23:56:18 +0700

    Unique Paths II

--- DP/Unique Paths II/solution.py
+++ DP/Unique Paths II/solution.py
@@ -2,12 +2,13 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        map = [[0] * (n + 1) for i in range(m+1)]
+        dp = [[0] * (n + 1) for i in range(m+1)]
+        
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                if obstacleGrid[i-1][j-1] == 0:
-                    if (i == 1 and j == 1):
-                        map[i][j] = 1
+                if obstacleGrid[i-1][j-1] == 0:     # space allow to walk to 
+                    if (i == 1 and j == 1):         # start point
+                        dp[i][j] = 1                
                     else:
-                        map[i][j] = map[i - 1][j] + map[i][j - 1]
-        return map[m][n]
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m][n]
