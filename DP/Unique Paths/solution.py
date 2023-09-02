e78718aa5623ec9f7b9edb1328d4bbf43ca9b559
Phong Phi Gia <tpw284@gmail.com>
2023-09-02 21:28:14 +0700

    Unique Paths

--- DP/Unique Paths/solution.py
+++ DP/Unique Paths/solution.py
@@ -1,3 +1,4 @@
+# 2D DP solution
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[1] * n for _ in range(m)]
@@ -7,3 +8,22 @@ class Solution:
                 dp[i][j] = dp[i][j-1] + dp[i-1][j]
     
         return dp[m-1][n-1]    
+    
+
+# 1D DP solution
+def uniquePaths(self, m: int, n: int) -> int:
+        curr_row = [1] * n
+        prev_row = [1] * n
+
+        for i in range(1, m):
+            for j in range(1, n):
+                curr_row[j] = curr_row[j - 1] + prev_row[j]    
+            curr_row, prev_row = prev_row, curr_row
+        
+        return prev_row[-1]
+
+
+# Math solution
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return math.comb(m+n-2, m-1)
