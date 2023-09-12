8894b93ec7038d270159a84b9f0bff681898d68e
Phong Phi Gia <tpw284@gmail.com>
2023-09-12 20:42:06 +0700

    Count All Valid Pickup and Delivery Options

--- DP/Count All Valid Pickup and Delivery Options/solution.py
+++ DP/Count All Valid Pickup and Delivery Options/solution.py
@@ -1,7 +1,14 @@
 mod = 10 ** 9 + 7
 class Solution:
     def countOrders(self, n: int) -> int:
-        res = 1
+        '''
+            For every new pair (Pn , Dn), we have (2i + 1) place to place Pn between (Pi, Di) with i from 1 to n-1
+            With each place of Dn, we can place Dn in 1, 2, ....., 2i + 1 place after Pn
+            Therefore, we have 1 + 2 + ... + (2i + 1) = (2i + 1)(i + 1) way to place new (Pn, Dn) pair
+            So, dp[i + 1] = dp[i] * (2i + 1) * (i + 1)
+        '''
+
+        res = 1                                             
         for i in range (n):
             res = res * (2* i + 1) * (i + 1) % mod
         return res
