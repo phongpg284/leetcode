ee3b469f15c52edddb92e0c0e2ec388914c42a4f
Phong Phi Gia <tpw284@gmail.com>
2023-09-18 23:41:00 +0700

    The K Weakest Rows in a Matrix

---
+++ Heap/The K Weakest Rows in a Matrix/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        heap = []
+        for i, row in enumerate(mat):
+            strength = sum(row)
+            heapq.heappush(heap, (-strength, -i))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return [-i for _, i in sorted(heap, reverse=True)]
