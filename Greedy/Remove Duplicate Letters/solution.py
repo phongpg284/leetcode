cfa3a7e859f351e332f18d928f4fa483d575c74a
Phong Phi Gia <tpw284@gmail.com>
2023-09-26 23:33:58 +0700

    Remove Duplicate Letters

--- Greedy/Remove Duplicate Letters/solution.py
+++ Greedy/Remove Duplicate Letters/solution.py
@@ -17,10 +17,12 @@ class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         res = []
         visited = set()
+        # last index of each character
         dp = {c: i for i, c in enumerate(s)}
-
+        
         for i, c in enumerate(s):
             if c not in visited:
+                # if current character < prev character, and that prev character appear later in string, pop it out
                 while res and c < res[-1] and i < dp[res[-1]]:
                     visited.remove(res.pop())
                 visited.add(c)
