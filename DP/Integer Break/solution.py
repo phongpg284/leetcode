5078354af6ba40db91ec9ee2b7c4d7b00607df8e
Phong Phi Gia <tpw284@gmail.com>
2023-10-06 21:23:45 +0700

    Integer Break

--- DP/Integer Break/solution.py
+++ DP/Integer Break/solution.py
@@ -14,3 +14,13 @@ class Solution:
                 res = max(res, i * self.breakNum(n - i, dp), i * (n - i))
         dp[n] = res
         return res
+
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        res = 1
+        for i in range(2, n + 1):
+            num = n // i
+            over_slot = n % i
+            res = max(res, ((num + 1) ** over_slot) * (num ** (i - over_slot)))
+        return res
