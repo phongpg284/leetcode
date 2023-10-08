f96096bfc4e7d00ecba07b109f5fa5da3e2690c7
Phong Phi Gia <tpw284@gmail.com>
2023-10-08 21:43:25 +0700

    Max Dot Product of Two Subsequences

--- DP/Max Dot Product of Two Subsequences/solution.py
+++ DP/Max Dot Product of Two Subsequences/solution.py
@@ -11,3 +11,21 @@ class Solution:
                 dp[i][j] = max(dp[i][j], curr_product, dp[i-1][j], dp[i][j-1], curr_product + dp[i-1][j-1])
         
         return dp[m][n]
+    
+
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+        
+        current = [float('-inf')] * (n + 1)
+        previous = [float('-inf')] * (n + 1)
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                curr_product = nums1[i-1] * nums2[j-1]
+                
+                current[j] = max(curr_product, previous[j], current[j-1], curr_product + max(0, previous[j-1]))
+            
+            current, previous = previous, current
+        
+        return previous[n]
