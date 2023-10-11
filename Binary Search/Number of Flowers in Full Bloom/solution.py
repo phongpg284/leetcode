f8a56e905e182ad80dd3a68f19ddd12428e6a127
Phong Phi Gia <tpw284@gmail.com>
2023-10-11 23:12:24 +0700

    Number of Flowers in Full Bloom

---
+++ Binary Search/Number of Flowers in Full Bloom/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        n = len(flowers)
+        start = sorted([start for start, end in flowers])
+        end = sorted([end for start, end in flowers])
+        res = []
+
+        def binary_search(arr, target):
+            left, right = 0, n - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if arr[mid] == target and (mid == n - 1 or arr[mid + 1] > arr[mid]):
+                    return mid + 1
+                elif arr[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            return left
+        
+        # Number of flowers have bloomed - number of flowers have ended is the result of each time
+        for time in people:
+            res.append(binary_search(start, time) - binary_search(end, time - 1))
+
+        return res
