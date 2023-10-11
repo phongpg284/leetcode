2d652479624c8dbdd93d6de72571d2ad92403328
Phong Phi Gia <tpw284@gmail.com>
2023-10-11 23:17:56 +0700

    Number of Flowers in Full Bloom

--- Binary Search/Number of Flowers in Full Bloom/solution.py
+++ Binary Search/Number of Flowers in Full Bloom/solution.py
@@ -22,3 +22,12 @@ class Solution:
             res.append(binary_search(start, time) - binary_search(end, time - 1))
 
         return res
+
+# Python way to do things 
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        start = sorted([s for s, e in flowers])
+        end = sorted([e for s, e in flowers])
+        return [bisect_right(start, t) - bisect_left(end, t) for t in people]
+
+            
