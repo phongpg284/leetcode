08e66e9283afc7f100a534dc52778e60963fbc0e
Phong Phi Gia <tpw284@gmail.com>
2023-10-24 00:00:45 +0700

    Power of Four

--- Recursion/Power of Four/solution.py
+++ Recursion/Power of Four/solution.py
@@ -7,3 +7,16 @@ class Solution:
                 return False
             n //= 4
         return True
+    
+
+# Math solution 
+import math
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and math.log2(n) % 2 == 0
+    
+# Bitmask solution
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and n & (n - 1) == 0 and n % 3 == 1
+    
