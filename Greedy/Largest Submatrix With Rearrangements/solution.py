b57413ae2e87d87ff3a5e311b388704294d2a4c4
Phong Phi Gia <tpw284@gmail.com>
2023-11-26 23:53:42 +0700

    Largest Submatrix With Rearrangements

---
+++ Greedy/Largest Submatrix With Rearrangements/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestSubmatrix(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        
+        for i in range(1, m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    matrix[i][j] += matrix[i - 1][j]
+        
+        res = 0
+        
+        for row in matrix:
+            row.sort(reverse=True)
+            for j in range(n):
+                res = max(res, row[j] * (j + 1))
+        
+        return res
