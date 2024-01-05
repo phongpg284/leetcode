b62fd21f00ad99c20125158bfe09b22f9f6978b9
Phong Phi Gia <tpw284@gmail.com>
2024-01-06 00:38:19 +0700

    Longest Increasing Subsequence

--- DP/Longest Increasing Subsequence/solution.py
+++ DP/Longest Increasing Subsequence/solution.py
@@ -9,3 +9,20 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
 
         return max(dp)
+    
+
+# binary search
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        temp = [nums[0]]
+        res = 1
+
+        for i in range(1, len(nums)):
+            if temp[-1] < nums[i]:
+                temp.append(nums[i])
+                res += 1
+            else:
+                ind = bisect.bisect_left(temp, nums[i])
+                temp[ind] = nums[i]
+
+        return res
