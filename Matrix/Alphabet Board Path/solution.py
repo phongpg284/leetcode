350f9614f001629911e68af9c1241a8e9fe6dc2c
Phong Phi Gia <tpw284@gmail.com>
2024-01-17 19:44:37 +0700

    Alphabet Board Path

--- Matrix/Alphabet Board Path/solution.py
+++ Matrix/Alphabet Board Path/solution.py
@@ -21,3 +21,37 @@ class Solution:
             return "!"
         next_r, next_c = self.get_pos(target[i + 1])
         return "!" + self.lookup(r1, c1, next_r, next_c, target, i + 1)
+
+
+# None recur
+class Solution:
+    def alphabetBoardPath(self, target: str) -> str:
+        i = 0
+        r1, c1 = 0, 0
+        path = ''
+
+        while i < len(target):
+            r2, c2 = self.get_pos(target[i])
+            if r2 < r1:
+                path += "U"
+                r1 -= 1
+                continue
+            if c2 < c1:
+                path += "L"
+                c1 -= 1
+                continue
+            if r2 > r1:
+                path += "D"
+                r1 += 1
+                continue
+            if c2 > c1:
+                path += "R"
+                c1 += 1
+                continue
+            path += '!'
+            i += 1
+        return path
+
+    def get_pos(self, c):
+        i = ord(c) - ord("a")
+        return i // 5, i % 5
