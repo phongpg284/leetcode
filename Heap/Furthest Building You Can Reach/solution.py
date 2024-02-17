04b1f5951b9dbe13380ea94dfbf0edbb9f9f9bdf
Phong Phi Gia <tpw284@gmail.com>
2024-02-17 21:42:35 +0700

    Furthest Building You Can Reach

--- Heap/Furthest Building You Can Reach/solution.py
+++ Heap/Furthest Building You Can Reach/solution.py
@@ -5,16 +5,16 @@ class Solution:
         for i in range(n - 1):
             dif = heights[i + 1] - heights[i]
             while dif > 0:
-                if bricks >= dif:
+                if bricks >= dif:               # store difference in max heap
                     bricks -= dif
                     heapq.heappush(s, -dif)
                     dif = 0
-                else:
-                    if ladders == 0:
+                else:                           # bricks not enout for this dif
+                    if ladders == 0:            
                         return i
 
-                    if len(s) > 0:
-                        top = s[0]
+                    if len(s) > 0:              
+                        top = s[0]              # pick the most dif in heap and replace that with a ladder
                         if dif > -top:
                             dif = 0
                         else:
