8a87460e525dac2e07cd7d804354f944ac288c07
Phong Phi Gia <tpw284@gmail.com>
2024-03-10 23:43:28 +0700

    Partition Labels

---
+++ Two Pointer/Partition Labels/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        n = len(s)
+        suffix_end = {s[i]: i for i in range(n)}
+        res = []
+        i = 0
+        while i < n:
+            j = i
+            max_end = suffix_end[s[j]]
+            while j < max_end:
+                j += 1
+                max_end = max(max_end, suffix_end[s[j]])
+            res.append(j - i + 1)
+            i = j + 1
+
+        return res
