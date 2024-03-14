317b20b2bab9dc84827197ff8ab0eb5e9467dce0
Phong Phi Gia <tpw284@gmail.com>
2024-03-14 21:59:32 +0700

    Binary Subarrays With Sum

--- PrefixSum/Binary Subarrays With Sum/solution.py
+++ PrefixSum/Binary Subarrays With Sum/solution.py
@@ -11,3 +11,26 @@ class Solution:
             count[t] = count.get(t, 0) + 1
 
         return res
+
+
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        count = {0: 1}
+        res = 0
+        t = 0
+
+        for num in nums:
+            if num == 1:
+                t += 1
+                count[t] = 1
+            else:
+                count[t] += 1
+
+        for num in count.keys():
+            if goal == 0:
+                res += count[num] * (count[num] - 1) // 2
+            else:
+                next = num + goal
+                if next in count:
+                    res += count[num] * count[next]
+        return res
