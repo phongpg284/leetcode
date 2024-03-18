5f1f53eb528917687ab1ab905190272e5c74d6bd
Phong Phi Gia <tpw284@gmail.com>
2024-03-19 00:40:58 +0700

    Minimum Moves to Equal Array Elements

---
+++ Math/Minimum Moves to Equal Array Elements/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minMoves(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 0
+
+        nums = sorted(nums)
+        res = 0
+        temp = 0
+        for i in range(1, len(nums)):
+            temp += nums[i] - nums[i - 1]
+            res += temp
+        return res
