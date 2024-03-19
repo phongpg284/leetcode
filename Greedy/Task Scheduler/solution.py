002910cd37e3d12f9d5b9236eeeb2989c9eb2276
Phong Phi Gia <tpw284@gmail.com>
2024-03-19 21:59:16 +0700

    Task Scheduler

--- Greedy/Task Scheduler/solution.py
+++ Greedy/Task Scheduler/solution.py
@@ -44,3 +44,20 @@ class Solution:
         return res
 
 
+## optimize no heap
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        freq = [0] * 26
+        for task in tasks:
+            freq[ord(task) - ord('A')] += 1
+        freq.sort()
+        chunk = freq[25] - 1                                ## each chunk consist all slot for all character -> number of chunk will be maximum frequency
+        idle = chunk * n                                    ## init idle as all other slot in total - 1 chunk 
+
+        for i in range(24, -1, -1):                         ## for every frequency, we can replace that amount of character instead of idle slot 
+            idle -= min(chunk, freq[i])
+
+        return len(tasks) + idle if idle >= 0 else len(tasks)       ## result will be len(tasks) + slots that need to be idle
+
+
+
