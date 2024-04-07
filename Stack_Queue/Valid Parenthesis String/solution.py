6407e1872bc0d9d60c5088b926fc71139c43eea4
Phong Phi Gia <tpw284@gmail.com>
2024-04-07 23:57:25 +0700

    Valid Parenthesis String

--- Stack_Queue/Valid Parenthesis String/solution.py
+++ Stack_Queue/Valid Parenthesis String/solution.py
@@ -21,5 +21,23 @@ class Solution:
             top_asterisk_index = asterisk.pop()
             if top_parent_index > top_asterisk_index:
                 return False
-        
+
         return len(open_parent) == 0
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftMin, leftMax = 0, 0
+
+        for c in s:
+            if c == "(":
+                leftMin, leftMax = leftMin + 1, leftMax + 1
+            elif c == ")":
+                leftMin, leftMax = leftMin - 1, leftMax - 1
+            else:
+                leftMin, leftMax = leftMin - 1, leftMax + 1
+            if leftMax < 0:
+                return False
+            if leftMin < 0:
+                leftMin = 0
+        return leftMin == 0
