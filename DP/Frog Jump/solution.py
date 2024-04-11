ddf16498cc6a724f15926ec1ea417ca283f8b660
Phong Phi Gia <tpw284@gmail.com>
2024-04-12 00:01:37 +0700

    Frog Jump

--- DP/Frog Jump/solution.py
+++ DP/Frog Jump/solution.py
@@ -1,32 +1,32 @@
 class Solution:
     def canCross(self, stones: List[int]) -> bool:
-        m = {}
         n = len(stones)
+        store = {stones[i]: i for i in range(n)}
         dp = [[-1] * n for _ in range(n)]
-
-        def solve(i, k):
-            if i == n - 1:
-                return True
-
-            if dp[i][k] != -1:
-                return dp[i][k] == 1
-
-            k0, kp, k1 = False, False, False
-
-            if stones[i] + k in m:
-                k0 = solve(m[stones[i] + k], k)
-            if k > 1 and stones[i] + k - 1 in m:
-                kp = solve(m[stones[i] + k - 1], k - 1)
-            if stones[i] + k + 1 in m:
-                k1 = solve(m[stones[i] + k + 1], k + 1)
-
-            dp[i][k] = 1 if k0 or kp or k1 else 0
-            return dp[i][k] == 1
 
         if stones[1] - stones[0] != 1:
             return False
+        return self.helper(store, dp, stones, 1, 1)
 
-        for i in range(n):
-            m[stones[i]] = i
+    def helper(self, store, dp, stones, i, k):
+        if i == len(stones) - 1:
+            return True
 
-        return solve(1, 1)
+        if dp[i][k] != -1:
+            return dp[i][k] == 1
+
+        k_cur, k_prev, k_next = False, False, False
+
+        if stones[i] + k in store:
+            k_cur = self.helper(store, dp, stones, store[stones[i] + k], k)
+
+        if k > 1 and stones[i] + k - 1 in store:
+            k_prev = self.helper(
+                store, dp, stones, store[stones[i] + k - 1], k - 1)
+
+        if stones[i] + k + 1 in store:
+            k_next = self.helper(
+                store, dp, stones, store[stones[i] + k + 1], k + 1)
+
+        dp[i][k] = 1 if k_cur or k_prev or k_next else 0
+        return dp[i][k] == 1
