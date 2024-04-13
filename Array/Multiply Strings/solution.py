facad4bffd98455d9a26d6f368b25cfec61ec4c6
Phong Phi Gia <tpw284@gmail.com>
2024-04-13 23:36:08 +0700

    Multiply Strings

--- Array/Multiply Strings/solution.py
+++ Array/Multiply Strings/solution.py
@@ -1,21 +1,18 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num1 = num1[::-1]
-        num2 = num2[::-1]
-        len_1 = len(num1)
-        len_2 = len(num2)
+        n1 = len(num1)
+        n2 = len(num2)
         count = 0
         res = ""
-        for i in range(len_1 + len_2):
+        for i in range(n1 + n2):
             cur = 0
-            for j in range(0, i + 1):
-                if j >= len_1 or (i-j) >= len_2:
-                    continue
-                cur += int(num1[j]) * int(num2[i-j])
+            for j in range(i + 1):
+                if j < n1 and (i - j) < n2:
+                    cur += int(num1[n1 - 1 - j]) * int(num2[n2 - 1 - i + j])
 
             cur += count
             count = cur // 10
             res += str(cur % 10)
-        while res[-1] == "0" and len(res) > 1:
-            res = res[:len(res) - 1]
-        return res[::-1]
+
+        res = res[::-1].lstrip('0')
+        return res if res else '0'
