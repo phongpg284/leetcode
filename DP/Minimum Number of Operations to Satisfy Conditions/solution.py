448e697af912b5154169861bf844347a90631c53
Phong Phi Gia <tpw284@gmail.com>
2024-04-23 00:10:51 +0700

    Minimum Number of Operations to Satisfy Conditions

--- DP/Minimum Number of Operations to Satisfy Conditions/solution.py
+++ DP/Minimum Number of Operations to Satisfy Conditions/solution.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 val = grid[i][j]
-                count[j][val] += 1
+                count[j][val] += 1          # add count number of value in col j
 
         for i in range(n):
             for j in range(10):
@@ -17,7 +17,7 @@ class Solution:
                     min_val = float('inf')
                     for k in range(10):
                         if k != j:
-                            min_val = min(dp[i - 1][k], min_val)
+                            min_val = min(dp[i - 1][k], min_val)            # get min operation from previous col that different current col
                     dp[i][j] = min_val + m - count[i][j]
 
         return min(dp[i])
