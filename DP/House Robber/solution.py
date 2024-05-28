0675d2fee6ffe3bd78295a0d3f72dbf178764c2b
Phong Phi Gia <tpw284@gmail.com>
2024-05-29 00:42:50 +0700

    House Robber

--- DP/House Robber/solution.py
+++ DP/House Robber/solution.py
@@ -1,3 +1,4 @@
+# top down
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
@@ -16,3 +17,15 @@ class Solution:
                   self.helper(dp, nums, n - 2) + nums[n])
         dp[n] = res
         return res
+
+
+# bottom up
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * (n + 1)
+        dp[0] = nums[0] 
+        for i in range(1, n):
+            prev = dp[i - 2] if i >= 2 else 0
+            dp[i] = max(dp[i - 1], nums[i] + prev)
+        return dp[n - 1]
