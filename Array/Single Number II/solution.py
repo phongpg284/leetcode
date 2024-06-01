e56c0d536038d362f02c1e6b2114c4c52a7c8d2f
Phong Phi Gia <tpw284@gmail.com>
2024-06-01 23:57:03 +0700

    Single Number II

--- Array/Single Number II/solution.py
+++ Array/Single Number II/solution.py
@@ -1,15 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        nums = sorted(nums)
-        for i in range(len(nums)):
-            if i == 0:
-                if nums[i] != nums[i + 1]:
-                    return nums[0]
-            elif i == len(nums) - 1:
-                if nums[i] != nums[i-1]:
-                    return nums[i]
-            else:
-                if nums[i] != nums[i + 1] and nums[i] != nums[i - 1]:
-                    return nums[i]
+        counter = Counter(nums)
+        for key, count in counter.items():
+            if count == 1:
+                return key
